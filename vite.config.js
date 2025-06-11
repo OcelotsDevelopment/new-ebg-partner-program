7a6145c70021efb05b086c1ad652f356e581aadd
robinOcelots <robin@ocelots.in>
2025-06-11 11:18:45 +0530

    push some dev dependency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'aboutus.html',
+          dest: '.'
+        }
+      ]
+    })
   ],
 })
